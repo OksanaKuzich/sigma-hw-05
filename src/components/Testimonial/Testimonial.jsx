9106326856d363dba49b810f5e5bbeedf3a4b017
OksanaKuzich <komoks255@gmail.com>
2023-03-28 22:23:41 +0200

    fix json

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,24 +1,24 @@
 import { Subtitle } from 'components/Home/Subtitle/Subtitle';
 import { Title } from 'components/Home/Title/Title';
 
-const allTestimonial = [
-  {
-    number: '100%',
-    title: 'Organic',
-  },
-  {
-    number: '285',
-    title: 'Active Product',
-  },
-  {
-    number: '350+',
-    title: 'Organic Orchads',
-  },
-  {
-    number: '25+',
-    title: 'Years of Farming',
-  },
-];
+// const allTestimonial = [
+//   {
+//     number: '100%',
+//     title: 'Organic',
+//   },
+//   {
+//     number: '285',
+//     title: 'Active Product',
+//   },
+//   {
+//     number: '350+',
+//     title: 'Organic Orchads',
+//   },
+//   {
+//     number: '25+',
+//     title: 'Years of Farming',
+//   },
+// ];
 
 export const Testimonial = () => {
   return (
