37c8537254dc20c36bbb8f5700925734be017237
OksanaKuzich <komoks255@gmail.com>
2023-04-05 14:55:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { CartProvider } from './hooks/useCart';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
       <ThemeProvider theme={theme}>
         <CartProvider>
           <App />
